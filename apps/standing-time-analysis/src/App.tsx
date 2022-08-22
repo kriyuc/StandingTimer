de39aa0f7e9fcd773774941c785d01dac39f641f
Dante Poleselli <dante.poleselli@slalom.com>
2022-08-22 16:59:14 -0700

    create react client

---
+++ apps/standing-time-analysis/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <header style={{ height: 60, background: "blue" }}>
+        <h1>Welcome to Standing Time Analysis</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
